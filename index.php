a067f984a5e9da571cf336297a0b1c9f272736ca
Julio Alberto Lemus <julioalberto.lemus@galileo.edu>
2024-03-08 02:06:59 +0000

    Carga de arcivos php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center><h1>Hola mundo!!! <br> Mi nombre es: Julio Lemus :)</h1></center>";
+?>
+    <br>
+    <div align="center">
+<img src="image.jpg" alt="Paisaje Nordico" >
+</div>
